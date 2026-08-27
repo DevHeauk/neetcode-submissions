4602a510433a54eb0b9e1cfc4b8cab108b966ab8
HeaukJun Yoo <tianfla24@gmail.com>
2026-08-28 05:02:00 +0900

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.ts
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number[]}
+     */
+    countBits(n: number): number[] {
+        const result = [];
+        // ans[i] = ans[i & i-1] + 1
+        const ans = [];
+        for (let i =0; i <= n; i++) {
+            let count = 0;
+            let num = i;
+            if (i > 0 && ans[i & i - 1]) {
+                ans[i] = ans[i & i - 1] + 1;
+                result.push(count);
+                continue;
+            }
+
+            while (num > 0) {
+                num = num & (num-1);
+                count++; 
+            }
+            ans[i] = 0;
+            result.push(count); 
+        }
+        return result
+    }
+}
